6a0e54eb1a584aee45ee6ef7d6ad659753b79350
sampanchan <spankratz@my.creativecircus.edu>
2021-06-01 13:05:56 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+console.log('Hello World from main.js!');
+
+var lineAnimation = function lineAnimation() {
+  gsap.to('.rectangle', {
+    duration: 0.05,
+    y: "10%",
+    repeat: 1,
+    yoyo: true,
+    stagger: {
+      //   grid: [7,15],
+      from: "random",
+      amount: 1.2
+    },
+    ease: "none",
+    onComplete: lineAnimation
+  });
+};
+
+lineAnimation();
+gsap.to(".half-circle-one", {
+  duration: 15,
+  rotation: "360_cw",
+  repeat: -1,
+  // yoyo: true,
+  ease: 'none',
+  transformOrigin: "bottom center"
+});
+gsap.to(".half-circle-two", {
+  duration: 15,
+  rotation: "360_ccw",
+  repeat: -1,
+  // yoyo: true,
+  ease: 'none',
+  transformOrigin: "bottom center"
+});
+gsap.to(".circle-one", {
+  duration: 1,
+  x: "20%",
+  repeat: -1,
+  yoyo: true,
+  delay: 2
+});
+gsap.to(".circle-three", {
+  duration: 1,
+  x: "-20%",
+  repeat: -1,
+  yoyo: true,
+  delay: 4
+});
+gsap.to(".circle-four", {
+  duration: 1,
+  y: "-20%",
+  repeat: -1,
+  yoyo: true,
+  delay: 4
+});
+//# sourceMappingURL=main.js.map
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Web Animations Poster Starter
+
+Serves as a starting point for web animations classes to create highly animated posters in a horizontally and vertically responsive, proportioned frame.
